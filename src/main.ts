dca2ca2d77d55383bb537379ed29aa737cb94263
whitehack <whitehack@163.com>
2020-07-20 14:57:20 +0800

    update typescript to 3.5.3

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-
-
-import {resolve} from 'path';
-import * as TJS from "typescript-json-schema";
-import * as fs from 'fs';
-import {Definition} from "typescript-json-schema";
-import * as util from 'util';
 import * as path from 'path';
-import {JsonSchemaGenerator} from "typescript-json-schema";
+import { resolve } from 'path';
+import * as TJS from "typescript-json-schema";
+import { Definition, JsonSchemaGenerator } from "typescript-json-schema";
+import * as fs from 'fs';
+import * as util from 'util';
 
-function log(...args){
+function log(...args) {
     console.log(...args);
 }
 
-function error(msg,...args){
-    const str = util.format(msg,...args);
+function error(msg, ...args) {
+    const str = util.format(msg, ...args);
     console.error(str);
     throw new Error(str);
 }
@@ -21,17 +18,17 @@ function error(msg,...args){
 let responseStr = '_Res';
 let requestStr = '_Req';
 
-export function parseToPinusProtobuf(baseDir:string,reqStr='_Req',resStr='_Res'): {client:object,server:object}{
+export function parseToPinusProtobuf(baseDir: string, reqStr = '_Req', resStr = '_Res'): { client: object, server: object } {
     responseStr = resStr;
     requestStr = reqStr;
-    let retObj = {client:{},server:{}};
+    let retObj = { client: {}, server: {} };
     const files = fs.readdirSync(baseDir);
-    const tsFilePaths:string[] = [];
-    files.forEach(val=>{
-        if(!val.endsWith('.ts')){
+    const tsFilePaths: string[] = [];
+    files.forEach(val => {
+        if (!val.endsWith('.ts')) {
             return;
         }
-        tsFilePaths.push(resolve(baseDir+'/'+val));
+        tsFilePaths.push(resolve(baseDir + '/' + val));
         // const obj = parseFile(baseDir,val);
         // const tmp = path.parse(val);
         // retObj.client[tmp.name] = obj.client;
@@ -52,11 +49,11 @@ export function parseToPinusProtobuf(baseDir:string,reqStr='_Req',resStr='_Res')
     // all symbols
 
     const symbols = generator.getMainFileSymbols(program);
-    files.forEach(val=>{
-        if(!val.endsWith('.ts')){
+    files.forEach(val => {
+        if (!val.endsWith('.ts')) {
             return;
         }
-        const obj = parseFile(baseDir,val,program,generator,symbols);
+        const obj = parseFile(baseDir, val, program, generator, symbols);
         const tmp = path.parse(val);
         retObj.client[tmp.name] = obj.client;
         retObj.server[tmp.name] = obj.server;
@@ -64,92 +61,92 @@ export function parseToPinusProtobuf(baseDir:string,reqStr='_Req',resStr='_Res')
     return retObj;
 }
 
-function parseFile(baseDir:string,filename:string,program:TJS.Program,generator:JsonSchemaGenerator,symbols:string[]){
-    if(!symbols || !symbols.length){
+function parseFile(baseDir: string, filename: string, program: TJS.Program, generator: JsonSchemaGenerator, symbols: string[]) {
+    if (!symbols || !symbols.length) {
         return;
     }
     const filePath = path.parse(filename);
-    filename = filePath.name.replace(/\./g,'_');
+    filename = filePath.name.replace(/\./g, '_');
     // const symbolName = symbols[symbols.length-1];
     // if(!symbolName){
     //     return;
     // }
 
-    let  symbolClient;
-    if(symbols.includes(filename+requestStr)){
-        symbolClient = generator.getSchemaForSymbol(filename+requestStr);
+    let symbolClient;
+    if (symbols.includes(filename + requestStr)) {
+        symbolClient = generator.getSchemaForSymbol(filename + requestStr);
     }
     let client;
     let server;
-    if(symbolClient){
+    if (symbolClient) {
         const messages = {};
-        client =  parseSymbol(symbolClient,symbolClient,messages);
+        client = parseSymbol(symbolClient, symbolClient, messages);
     }
     let symbolServer;
-    if(symbols.includes(filename+responseStr)){
-        if(!client){
-            console.warn('WARNING:',filename,`has ${responseStr} without ${requestStr}`);
+    if (symbols.includes(filename + responseStr)) {
+        if (!client) {
+            console.warn('WARNING:', filename, `has ${ responseStr } without ${ requestStr }`);
         }
-        symbolServer = generator.getSchemaForSymbol(filename+responseStr);
+        symbolServer = generator.getSchemaForSymbol(filename + responseStr);
     }
-    if(!symbolServer){
-        if(client){
-         //   console.warn('WARNING:',filename,`has ${requestStr} without ${responseStr}`);
+    if (!symbolServer) {
+        if (client) {
+            //   console.warn('WARNING:',filename,`has ${requestStr} without ${responseStr}`);
         }
-        if(symbols.includes(filename)){
+        if (symbols.includes(filename)) {
             symbolServer = generator.getSchemaForSymbol(filename);
         }
     }
-    if(!symbolServer){
-        return {client:client};
+    if (!symbolServer) {
+        return { client: client };
     }
     const messages = {};
-    server =  parseSymbol(symbolServer,symbolServer,messages);
-    return {client:client,server:server};
- //   return transMessage(obj,messages);
+    server = parseSymbol(symbolServer, symbolServer, messages);
+    return { client: client, server: server };
+    //   return transMessage(obj,messages);
 }
 
 
-
-interface IProp{
-    $ref?:string;
-    type?:string;
-    additionalProperties?:string;
-    items:{type?:string;$ref?:string};
+interface IProp {
+    $ref?: string;
+    type?: string;
+    additionalProperties?: string;
+    items: { type?: string; $ref?: string };
 }
 
 const PROTOBUF_TYPES = [
     "uInt32",
     "sInt32",
-    "int32" ,
+    "int32",
     "double",
     "string",
     "message",
     "float"
 ];
-function getDefinitionFromRoot(root:Definition,ref:string):Definition{
+
+function getDefinitionFromRoot(root: Definition, ref: string): Definition {
     // "#/definitions/MyRank"
-    let name:string = ref.split('/') as any;
-    name = name[name.length-1];
-    const ret =  root.definitions[name];
-    if(!ret){
-        error('!find definition from root error',root,ret);
+    let name: string = ref.split('/') as any;
+    name = name[name.length - 1];
+    const ret = root.definitions[name];
+    if (!ret) {
+        error('!find definition from root error', root, ret);
     }
-    ret.name = name;
-    return ret;
+    ret['name'] = name;
+    return ret as Definition;
 }
 
-function normalType(typeName:string){
-    if(PROTOBUF_TYPES.includes(typeName)){
+function normalType(typeName: string) {
+    if (PROTOBUF_TYPES.includes(typeName)) {
         return typeName;
     }
-    if(typeName=='number'){
+    if (typeName == 'number') {
         return 'uInt32';
     }
-    error('!! error typeName',typeName);
+    error('!! error typeName', typeName);
 }
 
-function parseSymbol(root:Definition,symbol:Definition,messages:object){
+function parseSymbol(root: Definition, symbol: Definition, messages: object) {
     /*
 {
   "type": "object",
@@ -214,58 +211,59 @@ function parseSymbol(root:Definition,symbol:Definition,messages:object){
   "$schema": "http://json-schema.org/draft-06/schema#"
 }
      */
-    function parseRef(obj,key,$ref){
-        const definition = getDefinitionFromRoot(root,$ref);
+    function parseRef(obj, key, $ref) {
+        const definition = getDefinitionFromRoot(root, $ref);
         const name = (definition as any).name;
-        if(definition.enum){
-            if(!definition.type){
-                console.log(obj,key,$ref,definition,name);
+        if (definition.enum) {
+            if (!definition.type) {
+                console.log(obj, key, $ref, definition, name);
                 throw new Error('!! un know enum type');
             }
             let type = definition.type;
-            if(type=='number' || type=='boolean'){
+            if (type == 'number' || type == 'boolean') {
                 type = 'uInt32';
             }
-            return ' '+ type+' '+key;
+            return ' ' + type + ' ' + key;
         }
-        if(!messages[name]){
-            messages[name] = parseSymbol(root,definition,messages);
+        if (!messages[name]) {
+            messages[name] = parseSymbol(root, definition, messages);
         }
-        obj['message '+ name] = messages[name];
-        return ' ' +name+' '+key;
+        obj['message ' + name] = messages[name];
+        return ' ' + name + ' ' + key;
     }
+
     let val = {};
     let i = 1;
-    for(let key in symbol.properties){
+    for (let key in symbol.properties) {
         const prop = <IProp>symbol.properties[key];
         let msgkey = 'optional';
         // 判断是否是required
-        if(prop.type=='array'){
+        if (prop.type == 'array') {
             msgkey = 'repeated';
-        }else if(symbol.required && symbol.required.includes(key)){
+        } else if (symbol.required && symbol.required.includes(key)) {
             msgkey = 'required';
         }
         // 判断类型  type items additionalProperties
-        if(prop.type!='array'){
-            if(prop.$ref){
-                if(prop.type){
-                    msgkey+=' '+normalType(prop.type)+' '+key;
-                }else{
-                    msgkey += parseRef(val,key,prop.$ref);
+        if (prop.type != 'array') {
+            if (prop.$ref) {
+                if (prop.type) {
+                    msgkey += ' ' + normalType(prop.type) + ' ' + key;
+                } else {
+                    msgkey += parseRef(val, key, prop.$ref);
                 }
 
-            }else{
-                msgkey+=' '+normalType(prop.type)+' '+key;
+            } else {
+                msgkey += ' ' + normalType(prop.type) + ' ' + key;
             }
-        }else{
+        } else {
             // array
-            if(prop.additionalProperties){
-                msgkey += ' '+normalType(prop.additionalProperties)+' ' + key;
-            }else{
-                if(prop.items.type){
-                    msgkey += ' ' +normalType(prop.items.type)+ ' ' + key;
-                }else{
-                    msgkey += parseRef(val,key,prop.items.$ref);
+            if (prop.additionalProperties) {
+                msgkey += ' ' + normalType(prop.additionalProperties) + ' ' + key;
+            } else {
+                if (prop.items.type) {
+                    msgkey += ' ' + normalType(prop.items.type) + ' ' + key;
+                } else {
+                    msgkey += parseRef(val, key, prop.items.$ref);
                 }
             }
         }
